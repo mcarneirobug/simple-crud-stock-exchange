9e455a2775e715ff275382cb413f3be53c89efc0
Matheus Santos <mcarneiro@sga.pucminas.br>
2021-01-06 22:30:21 -0300

    refactor: operation repository

--- src/main/java/com/stock/exchange/modules/operation/service/impl/OperationRepository.java
+++ src/main/java/com/stock/exchange/modules/operation/service/impl/OperationRepository.java
@@ -2,15 +2,22 @@ package com.stock.exchange.modules.operation.service.impl;
 
 import com.stock.exchange.model.Operation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface OperationRepository extends JpaRepository<Operation, Long> {
 
-    Optional<Operation> findByUserName(String userName);
+    @Query("FROM Operation p " +
+            "WHERE p.userName = :userName")
+    List<Operation> findByUserName(@Param("userName") String userName);
 
-    Optional<Operation> findByCorretora(String corretora);
+    @Query("FROM Operation p "
+            + "WHERE p.corretora = :corretora")
+    List<Operation> findByCorretora(@Param("corretora") String corretora);
 
 }
