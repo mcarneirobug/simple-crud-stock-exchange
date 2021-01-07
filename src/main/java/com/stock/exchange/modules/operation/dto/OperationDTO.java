43a1892c5be1f6992e0f4bf3714a7bb42023480f
Matheus Santos <mcarneiro@sga.pucminas.br>
2021-01-06 22:31:04 -0300

    feat: added swagger property to DTO

--- src/main/java/com/stock/exchange/modules/operation/dto/OperationDTO.java
+++ src/main/java/com/stock/exchange/modules/operation/dto/OperationDTO.java
@@ -1,8 +1,10 @@
 package com.stock.exchange.modules.operation.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,26 +25,32 @@ public class OperationDTO implements Serializable {
 
     private static final long serialVersionUID = 7309218902400427594L;
 
+    @JsonIgnore
     private Long id;
 
+    @ApiModelProperty(value = "Nome do usuário.", required = true, example = "mcarneiro")
     @NotBlank
     @Size(min = 1, max = 200)
     private String userName;
 
+    @ApiModelProperty(value = "Nome da corretora", required = true, example = "rico")
     @NotBlank
     @Size(min = 1, max = 200)
     private String corretora;
 
+    @ApiModelProperty(value = "Data de operação", required = true, example = "2021-01-05")
     @NotNull
     @JsonSerialize(using = LocalDateSerializer.class)
     @JsonFormat(pattern = "yyyy-MM-dd")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
 
+    @ApiModelProperty(value = "Robô que realizou a operação", required = true, example = "mrRobot")
     @NotNull
     @Size(min = 1, max = 200)
     private String robo;
 
+    @ApiModelProperty(value = "Valor da operação.", required = true, example = "200.0")
     private Double valor;
 
 }
